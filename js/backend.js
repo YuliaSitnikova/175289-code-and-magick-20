a4a4f246f9a2d9ef6bb249c070c0a8573d5aedb5
Юлия Ситникова <agrael123@yandex.ru>
2020-07-13 14:49:37 +0400

    Исправление синтаксических ошибок

--- js/backend.js
+++ js/backend.js
@@ -7,7 +7,7 @@
   };
   var load = function (onLoad, onError) {
     var URL = 'https://javascript.pages.academy/code-and-magick/data';
-    var xhr = new XMLHttpRequest;
+    var xhr = new XMLHttpRequest();
     xhr.addEventListener('load', function () {
       if (xhr.status === statusCode.OK) {
         onLoad(xhr.response);
@@ -28,7 +28,7 @@
   };
   var save = function (data, onLoad, onError) {
     var URL = 'https://javascript.pages.academy/code-and-magick';
-    var xhr = new XMLHttpRequest;
+    var xhr = new XMLHttpRequest();
     xhr.addEventListener('load', function () {
       if (xhr.status === statusCode.OK) {
         onLoad();
