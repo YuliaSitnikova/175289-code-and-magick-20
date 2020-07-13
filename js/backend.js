e6399d2a3ab537e5c00f06307843b7bbc29430b5
Юлия Ситникова <agrael123@yandex.ru>
2020-07-13 14:45:50 +0400

    Реализует сохранение и получение данных

---
+++ js/backend.js
@@ -0,0 +1,54 @@
+'use strict';
+
+(function () {
+  var TIMEOUT = 10000;
+  var statusCode = {
+    OK: 200,
+  };
+  var load = function (onLoad, onError) {
+    var URL = 'https://javascript.pages.academy/code-and-magick/data';
+    var xhr = new XMLHttpRequest;
+    xhr.addEventListener('load', function () {
+      if (xhr.status === statusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+    xhr.responseType = 'json';
+    xhr.timeout = TIMEOUT;
+    xhr.open('GET', URL);
+    xhr.send();
+  };
+  var save = function (data, onLoad, onError) {
+    var URL = 'https://javascript.pages.academy/code-and-magick';
+    var xhr = new XMLHttpRequest;
+    xhr.addEventListener('load', function () {
+      if (xhr.status === statusCode.OK) {
+        onLoad();
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+    xhr.responseType = 'json';
+    xhr.timeout = TIMEOUT;
+    xhr.open('POST', URL);
+    xhr.send(data);
+  };
+  window.backend = {
+    load: load,
+    save: save,
+  };
+})();
