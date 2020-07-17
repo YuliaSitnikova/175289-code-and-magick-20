4cd5bf82b5fe7b9f2703994d3559a382c392075d
Юлия Ситникова <Agrael123@yandex.ru>
2020-07-17 11:16:57 +0500

    Выносит повторяющийся код в отдельную функцию

--- js/backend.js
+++ js/backend.js
@@ -5,8 +5,8 @@
   var statusCode = {
     OK: 200,
   };
-  var load = function (onLoad, onError) {
-    var URL = 'https://javascript.pages.academy/code-and-magick/data';
+
+  var createXHR = function (url, method, data, onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.addEventListener('load', function () {
       if (xhr.status === statusCode.OK) {
@@ -23,30 +23,22 @@
     });
     xhr.responseType = 'json';
     xhr.timeout = TIMEOUT;
-    xhr.open('GET', URL);
-    xhr.send();
-  };
-  var save = function (data, onLoad, onError) {
-    var URL = 'https://javascript.pages.academy/code-and-magick';
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', function () {
-      if (xhr.status === statusCode.OK) {
-        onLoad();
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-    xhr.responseType = 'json';
-    xhr.timeout = TIMEOUT;
-    xhr.open('POST', URL);
+    xhr.open(method, url);
     xhr.send(data);
   };
+
+  var load = function (onLoad, onError) {
+    var URL = 'https://javascript.pages.academy/code-and-magick/data';
+    var method = 'GET';
+    createXHR(URL, method, null, onLoad, onError);
+  };
+
+  var save = function (data, onLoad, onError) {
+    var URL = 'https://javascript.pages.academy/code-and-magick';
+    var method = 'POST';
+    createXHR(URL, method, data, onLoad, onError);
+  };
+
   window.backend = {
     load: load,
     save: save,
